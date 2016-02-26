7d8e9ab44bc59825e764aea09a173c4d9c50e6ad
Nate Butler <iamnbutler@gmail.com>
2016-02-25 22:33:07 -0500

    update

---
+++ python/pyTest.py
@@ -0,0 +1,13 @@
+# Explorer Python (Nathan Butler | iamnbutler@gmail.com)
+# Run apt-get install arduino before running this
+
+# Setup
+import picamera
+camera 							= 	picamera.PiCamera()				# Set up instance of camera
+camera.brightness 	= 	60 												# Adjust Brightness
+# camera.hflip 			= 	True
+# camera.vflip 			= 	True
+
+camera.capture(uuid.uuid4 + '_loc_' + locID + '.jpg')	# Name image with unique identifier + a location number
+
+time.sleep(5.0)
