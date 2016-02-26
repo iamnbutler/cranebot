f975693c02b734f9c58bc76d4394d8f35fd1d100
Nate Butler <iamnbutler@gmail.com>
2016-02-26 00:01:17 -0500

    import time

--- python/pyTest.py
+++ python/pyTest.py
@@ -4,6 +4,7 @@
 # Setup
 import picamera
 import uuid
+import time
 camera 							= 	picamera.PiCamera()				# Set up instance of camera
 camera.brightness 	= 	60 												# Adjust Brightness
 # camera.hflip 			= 	True
