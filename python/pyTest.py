59cbe134f68136d3a71738a7535b4401d75fed66
Nate Butler <iamnbutler@gmail.com>
2016-02-25 23:50:14 -0500

    push

--- python/pyTest.py
+++ python/pyTest.py
@@ -7,9 +7,6 @@ camera 							= 	picamera.PiCamera()				# Set up instance of camera
 camera.brightness 	= 	60 												# Adjust Brightness
 # camera.hflip 			= 	True
 # camera.vflip 			= 	True
-int x = 0;
 
-while (x > 20):
-  camera.capture(uuid.uuid1() + '_test_' + x + '.jpg')
-  x = x + 1
-  time.sleep(2)
+camera.capture(uuid.uuid1() + '_test_' + x + '.jpg')
+time.sleep(2)
