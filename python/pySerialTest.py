40a5fec5f85946e9978af96dbc01e386e15d4f61
Nate Butler <iamnbutler@gmail.com>
2016-04-01 00:47:26 -0400

    fix ardunio serial

--- python/pySerialTest.py
+++ python/pySerialTest.py
@@ -1,5 +1,5 @@
 import serial
-arduino = serial.Serial('COM1', 115200, timeout=.1)
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
