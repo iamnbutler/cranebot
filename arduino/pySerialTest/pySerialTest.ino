36c6b11a5a2e8f589bd1b5f6608890f7d7aea894
Nate Butler <iamnbutler@gmail.com>
2016-04-01 00:44:55 -0400

    Add piSerialTest.py

--- arduino/pySerialTest/pySerialTest.ino
+++ arduino/pySerialTest/pySerialTest.ino
@@ -1,5 +1,5 @@
 void setup() {
-	Serial.begin(115200); // use the same baud-rate as the python side
+	Serial.begin(9600); // use the same baud-rate as the python side
 }
 void loop() {
 	Serial.println("Hello world from Ardunio!"); // write a string
