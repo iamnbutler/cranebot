56928b60e1db45dee3fa09a75b63c4b41080b519
Nate Butler <iamnbutler@gmail.com>
2016-04-01 14:11:59 -0400

    Update python scripts

--- explorer/explorer.py
+++ explorer/explorer.py
@@ -23,21 +23,28 @@ locID 							= 	0													# Location ID of where photo is taken
 capture 						= 	0													# 0 is inactive, 1 is active capture
 dist								=		0													# Sensor distance to object
 
-# def cap():
-	# If capture is 1 then take a picture
-	# if (capture = 1):
-	# 	while (loops < 4)
-	# 		camera.capture('export/location_' + str(locID) + '_' + str(uuid.uuid1()) + '.jpg')
-	# 		loops = loops + 1
-	# 		sleep(1.5)				# Wait 1.5s before next photo
-	# 	locID = locID +1		# Increment location ID
-	# 	loops = 0						# Reset loops
-	# 	capture = 0					# Capture is finished
-
+# Watch Serial for distance values
 def read():
 	while True:
 		data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	if data:
-		print data
+		if data:
+			dist = int(data)
+			if dist > 16:
+				print dist
+			elif dist < 16:
+				capture = True
+
+# Capture a series of 3 images
+def cap():
+	if capture:
+		while loops < 3:
+			camera.capture('export/location_' + str(locID) + '_' + str(uuid.uuid1()) + '.jpg')
+			loops = loops + 1
+			sleep(1.5)				# Wait 1.5s before next photo
+		locID = locID +1		# Increment location ID
+		loops = 0						# Reset loops
+		capture = false			# Capture is finished
+	else:
+		return
 
 read()
--- python/pySerialTest.py
+++ python/pySerialTest.py
@@ -1,6 +1,25 @@
 import serial
-arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
-while True:
-	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	if data:
-		print data
+arduino = serial.Serial('/dev/cu.usbmodem1421', 9600, timeout=.1)
+def read():
+	while True:
+		data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+		if data:
+			dist = int(data)
+			if dist > 16:
+				print dist
+			elif dist < 16:
+				print "test"
+
+def cap():
+	if capture == TRUE:
+		while loops < 4:
+			camera.capture('export/location_' + str(locID) + '_' + str(uuid.uuid1()) + '.jpg')
+			loops = loops + 1
+			sleep(1.5)				# Wait 1.5s before next photo
+		locID = locID +1		# Increment location ID
+		loops = 0						# Reset loops
+		capture = false			# Capture is finished
+	else:
+		return
+
+read()
