850703115f44e6cf89acaa734d7743bb84506825
Nate Butler <iamnbutler@gmail.com>
2016-04-01 00:52:15 -0400

    Test some real content

--- arduino/pySerialTest/pySerialTest.ino
+++ arduino/pySerialTest/pySerialTest.ino
@@ -1,7 +1,25 @@
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+long rawDistance, duration;
+int distance = 0;
+int lastDistance = 0;
+
 void setup() {
 	Serial.begin(9600); // use the same baud-rate as the python side
+	// Assign Pins
+  pinMode(trigPin, OUTPUT); // RF Trig Pin
+  pinMode(echoPin, INPUT);	// RF Echo Pin
 }
 void loop() {
-	Serial.println("Hello world from Ardunio!"); // write a string
-	delay(1000);
+
+	digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  rawDistance = (duration/2) / 29.1; // Magic number to convert range to cm
+  Serial.println(rawDistance); // write a string
+
+	delay(988);
 }
